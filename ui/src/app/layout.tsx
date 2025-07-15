7aebc690937404885e79f58c391bfc9537aca4d1
KrushikeshS <krushikeshshelar@gmail.com>
2025-07-15 19:39:40 +0530

    Add Google Analytics tracking code

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  return (
+return (
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
@@ -25,6 +25,21 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚪</text></svg>"
         />
         }
+
+        {/* Google tag (gtag.js) - START */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2G9517KHJ6"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-2G9517KHJ6');
+            `,
+          }}
+        />
+        {/* Google tag (gtag.js) - END */}
+
       </head>
       <body className={inter.className}>
         <main className="min-h-screen bg-gray-50">
@@ -32,6 +47,6 @@ export default function RootLayout({
         </main>
       </body>
     </html>
-  )
+  );
 }
 
